823f96657e9ba76beb546c652c762547c0893658
Q W Yeow <46264045+qwyeow@users.noreply.github.com>
2019-01-05 18:53:59 +0800

    Update temperature.py

--- miscellaneous/temperature.py
+++ miscellaneous/temperature.py
@@ -18,5 +18,27 @@ print(all( t>0 for t in temperatures))
 print("Any day above 28 degree?")
 print(any(t>28 for t in temperatures))
 
+'''
+(base) :~/Documents/PluralSight/PythonFundamentals/PracticeAgain$ python temperature.py
 
+min =   2.0,            max =  13.0,            average =   9.0
+min =   2.0,            max =  14.0,            average =  10.0
+min =   3.0,            max =  15.0,            average =  10.7
+min =   7.0,            max =  15.0,            average =  12.0
+min =   9.0,            max =  17.0,            average =  14.0
+min =  10.0,            max =  21.0,            average =  17.0
+min =  11.0,            max =  22.0,            average =  18.0
+min =  12.0,            max =  22.0,            average =  18.7
+min =  10.0,            max =  23.0,            average =  18.3
+min =   9.0,            max =  22.0,            average =  17.3
+min =   8.0,            max =  20.0,            average =  15.7
+min =   8.0,            max =  18.0,            average =  14.3
+
+All days are above zero degree?
+True
+
+Any day above 28 degree?
+False
+
+'''  
     
