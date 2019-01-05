05b032069fa825dc990e366cf4017b574d7df59a
Q W Yeow <46264045+qwyeow@users.noreply.github.com>
2019-01-05 18:57:41 +0800

    Add files via upload

--- miscellaneous/temperature.py
+++ miscellaneous/temperature.py
@@ -1,22 +1,26 @@
 from itertools import chain, islice
-from generator import lucas
 
 tuesday = [2,2,3,7,9,10,11,12,10,9,8,8]   
 monday = [13,14,14,14,16,20,21,22,22,21,19,17]
 sunday = [12,14,15,15,17,21,22,22,23,22,20,18]
 
+# use zip()
 for temps in zip(sunday,monday,tuesday):
     print(f"min = {min(temps):5.1f},\
             max = {max(temps):5.1f},\
             average = {(sum(temps)/len(temps)):5.1f}")
 
-
+# use chain()
 temperatures = chain(sunday,monday,tuesday)
 print("All days are above zero degree?")
+
+#use all()
 print(all( t>0 for t in temperatures))
 
+#use any()
 print("Any day above 28 degree?")
 print(any(t>28 for t in temperatures))
+
 
 '''
 (base) :~/Documents/PluralSight/PythonFundamentals/PracticeAgain$ python temperature.py
@@ -41,4 +45,7 @@ Any day above 28 degree?
 False
 
 '''  
+    
+
+
     
