0a3ee85cdcb740d46ecf709a5c241965d8f3f283
Q W Yeow <46264045+qwyeow@users.noreply.github.com>
2019-02-07 19:53:45 +0800

    Update codilify.py

--- miscellaneous/codilify.py
+++ miscellaneous/codilify.py
@@ -17,3 +17,23 @@ def solution(N):
     if nums[-1] is not False:
         nums = nums[:-1]
     return max(len(i) for i in nums)
+
+```
+Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number 
+and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”
+```
+
+for i in range(1,101):
+    if i%(3*5) == 0:
+        print("fizzbuzz")
+        continue
+    if i%3 == 0:
+        print("fizz")
+        continue
+    if i%5 == 0:
+        print("buzz")
+        continue
+    else:
+        print(i)
+        
+
