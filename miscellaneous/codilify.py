28e4115cc673d71aa2d48ee550e53b21d21ae914
Q W Yeow <46264045+qwyeow@users.noreply.github.com>
2019-02-07 19:54:40 +0800

    Update codilify.py

--- miscellaneous/codilify.py
+++ miscellaneous/codilify.py
@@ -1,4 +1,4 @@
-'''
+```
 1) BinaryGap
 Find longest sequence of zeros in binary representation of an integer. 
 
@@ -9,7 +9,7 @@ The number 529 has binary representation 1000010001 and contains two binary gaps
 The number 20 has binary representation 10100 and contains one binary gap of length 1. 
 The number 15 has binary representation 1111 and has no binary gaps. 
 The number 32 has binary representation 100000 and has no binary gaps.
-'''
+```
 
 def solution(N):     
     _ ,_ , num = str(bin(N)).partition("b")
@@ -18,10 +18,10 @@ def solution(N):
         nums = nums[:-1]
     return max(len(i) for i in nums)
 
-'''
+```
 Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number 
 and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”
-'''
+```
 
 for i in range(1,101):
     if i%(3*5) == 0:
